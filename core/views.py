e148223460e3046648efa7f04b2995e82712ab9f
root <root@lists.panamsquad.com>
2018-06-25 14:43:21 +0200

    users are now forced to create a profile

--- core/views.py
+++ core/views.py
@@ -3,9 +3,16 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 
 @login_required
 def home(request):
+    try:
+        profile = request.user.profile
+    except:
+        return HttpResponseRedirect(reverse('profile:profile'))
     return render(request, 'core/home.html')
 
 def privacy(request):
