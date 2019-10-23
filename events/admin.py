ddd13b9ee9b6f75f47837b442e7eaa7a475c0494
Franklyn J. Mandale <la.mandale.rd@gmail.com>
2019-10-23 15:20:16 +0200

    fix: add event__name in admin interface

--- events/admin.py
+++ events/admin.py
@@ -6,8 +6,11 @@ class EventAdmin(admin.ModelAdmin):
     filter_horizontal = ('expected_members', )
 
 class AttendanceAdmin(admin.ModelAdmin):
-    list_display = ('member', 'attendance', 'proxy_to', 'accepted',)
-    
+    list_display = ('event__name', 'member', 'attendance', 'proxy_to', 'accepted',)
+    list_filter = ('event__name',)
+
+    def event__name(self, obj):
+        return str(obj.event)
 
 admin.site.register(Place)
 admin.site.register(Attendance, AttendanceAdmin)
